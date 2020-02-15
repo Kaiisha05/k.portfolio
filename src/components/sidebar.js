4af31a6461e7b2452d5c71652301dd310c52af7f
Kaiisha05 <Kaiisha@yahoo.com>
2020-02-15 14:13:53 -0500

    added links to github and linkedin

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -7,17 +7,17 @@ function Sidebar() {
             <div className="sidebar">
                 <ul>
                     <li><a href='/'><i class="fas fa-home"></i> Home</a></li>
-                    <li><a href='/about'><i class="fas fa-user"></i> About</a></li>
+                    <li><a href='#'><i class="fas fa-user"></i> Profile</a></li>
                     <li><a href="#"><i class="fas fa-address-card"></i> Contact</a></li>
                     <li><a href='/portfolio'><i class="fas fa-project-diagram"></i> Portfolio</a></li>
                     <li><a href="#"><i class="fas fa-blog"></i> Blog</a></li>
-                    <li><a href="#"><i class="fas fa-address-book"></i> Contact</a></li>
+                    <li><a href='/about'><i class="fas fa-address-book"></i>About</a></li>
                     <li><a href="#"><i class="fas fa-map-pin"></i> Map</a></li>
                 </ul>
                 <div className="social_media">
-                    <a href="#"><i className="fab fa-facebook-f"></i></a>
-                    <a href="#"><i className="fab fa-twitter"></i></a>
-                    <a href="#"><i className="fab fa-instagram"></i></a>
+                    <a href='https://github.com/Kaiisha05' target="_blank"><i className="fab fa-github-square"></i></a>
+                    <a href='https://www.linkedin.com/in/kaiishao/' target="_blank"><i className="fab fa-linkedin"></i></a>
+                    <a href="#"><i className="fab fa-google-drive"></i></a>
                 </div>
             </div>
             {/* <div className="main_content">
